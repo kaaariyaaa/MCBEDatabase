a94e9c9629ca6996c761b5d10adab579f318e2eb
Kariya <kariya2298@gmail.com>
2026-01-13 21:17:55 +0900

    refactor: 使われていない古いファイルフォルダを削除

--- lib/packs/behavior/scripts/lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { WorldSqlDatabase } from "./mcbe-db/database.js";
-export type { Row, SqlValue } from "./mcbe-db/types.js";
--- lib/packs/behavior/scripts/lib/mcbe-db/sql.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ParsedQuery, JsonValue, WhereNode } from "./types.js";
-export declare function parseWhereExpression(expression: string): WhereNode;
-export declare function parseSql(sql: string, params?: JsonValue[]): ParsedQuery;
